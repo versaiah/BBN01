bb1db10db3d8420f001c3156483f1aee11fec865
Versaiah Fang <versaiah@versaiahs-macbook-pro.local>
2016-07-28 16:11:30 +0800

    Initial Commit

---
+++ Embrace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Embrace
+//
+//  Created by Versaiah Fang on 7/28/16.
+//  Copyright © 2016 Versaiah Fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
